cb5c171ca4b2bc4f4e75ed175abc5c5b33dd9633
Saga-Fire <75118944+Saga-Fire@users.noreply.github.com>
2020-12-03 12:44:09 +0100

    Advanced commit

---
+++ Voiture.js
@@ -0,0 +1,12 @@
+let carMikhail = new Voiture("CG169TP", "noir", 1200, 90, 50.0, 5);
+// let carNissim = new Voiture("");
+//console.log(car1);
+carMikhail.setInsurance = true;
+carMikhail.repeindre("bleue");
+carMikhail.mettreEssence(45);
+carMikhail.seDeplacer(501, 45);
+console.log(carMikhail.toString());
+// console.log(car1.repeindre.toString());
+console.log(carMikhail.getBoardMessage);
+console.log(carMikhail);
+
--- main.js
+++ main.js
@@ -8,12 +8,12 @@ class Voiture{
         this.numberSeats = numberSeats;
         this.fewlQuantity = 5;
         this.insurance = false;
-        this.boardMessage = `Bienvenue\n`;
+        this.boardMessage = `Bienvenue`;
     };
 
     set setInsurance(value) {
         this.insurance = value;
-        this.boardMessage = `${this.boardMessage}Vous avez assuré le véhicule\n`;
+        this.boardMessage = `${this.boardMessage}Vous avez assuré le véhicule`;
     };
 
     get getBoardMessage() {
@@ -22,40 +22,44 @@ class Voiture{
 
     repeindre(newColor) {
         if ((newColor === this.color)) {
-            this.boardMessage += `Le véhicule est déjà de cette couleur\n`;
+            this.boardMessage = `Le véhicule est déjà de cette couleur`;
+            return true;
         } else {
             this.color = newColor;
-            this.boardMessage += `Vous avez repeint le véhicul en ${this.color}\n`;
+            this.boardMessage = `Vous avez repeint le véhicul en ${this.color}`;
+            return false;
         };
     };
 
     mettreEssence(addFewlQuantity) {
         if ((addFewlQuantity + this.fewlQuantity) <= this.tankCapacity) {
             this.fewlQuantity += addFewlQuantity;
-            this.boardMessage += `Vous avez ajoué ${this.fewlQuantity}L d'essence\n`;
+            this.boardMessage = `Vous avez ajoué ${this.fewlQuantity}L d'essence`;
+            return this.fewlQuantity;
         } else {
-            this.boardMessage += `Réservoir d'essence insuffisant : opération annulée\n`;
+            this.boardMessage = `Réservoir d'essence insuffisant : opération annulée`;
+            return this.fewlQuantity;
         };
     };
 
     seDeplacer(distanceTravel, averageSpeed) {
         let fewlTrip = this.fewlConsumption(distanceTravel, averageSpeed);
         if (this.fewlQuantity >= fewlTrip) {
-            this.boardMessage += `Trajet authorisé, ${fewlTrip}L d'essence sera consommé durant le trajet\n`;
+            this.boardMessage = `Trajet authorisé, ${fewlTrip}L d'essence sera consommé durant le trajet`;
         } else {
-            this.boardMessage += `Trajet refusé, pas assez d\'essence dans le réservoir\n`;
+            this.boardMessage = `Trajet refusé, pas assez d'essence dans le réservoir`;
         };
     };
 
     fewlConsumption(distanceTravel, averageSpeed) { 
-        if ((averageSpeed < 50)) {
-            return ((distanceTravel * 10) / 100);
-        } else if ((averageSpeed >= 50) && (averageSpeed < 90)) {
-            return ((distanceTravel * 5) / 100);
-        } else if ((averageSpeed >= 90) && (averageSpeed < 130)) {
-            return ((distanceTravel * 9) / 100);
+        if ((averageSpeed <= 50)) {
+            return (distanceTravel * 0.1);
+        } else if ((averageSpeed > 50) && (averageSpeed <= 90)) {
+            return (distanceTravel * 0.05);
+        } else if ((averageSpeed > 90) && (averageSpeed <= 130)) {
+            return (distanceTravel * 0.09);
         } else {
-            return ((distanceTravel * 12) / 100);
+            return (distanceTravel * 0.12);
         };
     };
 
